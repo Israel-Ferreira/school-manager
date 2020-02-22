067784d514ad6b56b1f9b7a44f4f914ba3359576
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-02-22 20:02:22 -0300

    Finish School Screen

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,2 +1,47 @@
+# frozen_string_literal: true
+
 class SchoolsController < ApplicationController
+  before_action :set_school, only: %i[show edit update destroy]
+
+  def index
+    has_name = params[:name].present?
+    @schools = has_name ? School.find_by_name(title: params[:name]) : School.all
+  end
+
+  def show; end
+
+  def new
+    @school = School.new
+  end
+
+  def edit; end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save!
+      redirect_to schools_path
+    else
+      flash[:error] = @school.errors
+      render :new
+    end
+  end
+
+  def update
+    if @school.update(school_params)
+      redirect_to school_path
+    else
+      flash[:error] = @school.errors
+      render :edit
+    end
+  end
+
+  private
+
+  def set_school
+    @school = School.find(params[:id])
+  end
+
+  def school_params
+    params.require(:school).permit(:title, :owner_email, :s_domain, :pitch)
+  end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -4,4 +4,23 @@ class School < ApplicationRecord
   has_many :courses
   has_many :students
   scope :find_by_name, ->(name) { find_by(title: name) }
+
+  validates :title, :owner_email, presence: true
+  
+  before_validation :valid_subdomain
+
+  def valid_subdomain
+    if s_domain.present?
+      errors.add(:s_domain, 'Subdomínio Inválido') unless subdomain_is_correct?
+    end
+  end
+
+  private
+
+  def subdomain_is_correct?
+    uri = URI.parser(s_domain)
+    uri.is_a?(URI::HTTP) && !uri.Host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
 end
